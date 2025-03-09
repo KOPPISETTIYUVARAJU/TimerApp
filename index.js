78e7197d33ecb8acdaab6a284194e654da5d1585
KOPPISETTIYUVARAJU <yuvarajukoppisetti.6593@gmail.com>
2025-03-09 17:53:52 +0530

    index.js

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
-const fullName = user => user.firstName + ' ' + user.lastName
-
-const user = {firstName: 'Rahul', lastName: 'Attuluri'}
-const element = (
-  <div>
-    <h1 className='greeting'>Hello {fullName(user)} !</h1>
-    <p className='paragraph'>Good to see you here!</p>
-  </div>
-)
-ReactDOM.render(element, document.getElementById('root'))
+let timer;
+        let seconds = 0;
+        let isRunning = false;
+        function formatTime(s) {
+            let hrs = Math.floor(s / 3600).toString().padStart(2, '0');
+            let mins = Math.floor((s % 3600) / 60).toString().padStart(2, '0');
+            let secs = (s % 60).toString().padStart(2, '0');
+            return `${hrs}:${mins}:${secs}`;
+        }
+        function startTimer() {
+            if (!isRunning) {
+                isRunning = true;
+                timer = setInterval(() => {
+                    seconds++;
+                    document.getElementById('timer').innerText = formatTime(seconds);
+                }, 1000);
+            }
+        }
+        function pauseTimer() {
+            isRunning = false;
+            clearInterval(timer);
+        }
+        function resetTimer() {
+            isRunning = false;
+            clearInterval(timer);
+            seconds = 0;
+            document.getElementById('timer').innerText = formatTime(seconds);
+        }
+        function setBreak() {
+            isRunning = false;
+            clearInterval(timer);
+            seconds = 300; // 5-minute break
+            document.getElementById('timer').innerText = formatTime(seconds);
+        }
